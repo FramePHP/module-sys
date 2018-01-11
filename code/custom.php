20e89789bf67384263c769b1c21970e3e47b10af
HilkiahMakemo <hilkiah.makemo@skywest.com>
2018-01-11 06:58:12 -0700

    Create custom.php

---
+++ code/custom.php
@@ -0,0 +1,8 @@
+<?php
+
+
+/**
+ * This can be any code you want to run on your application, e.g. class, helper functions, etc.
+ * If you add folder structure here, make sure to modify composer.json to reflect this change.
+ * You can also implement psr-4 standards if you choose, everything is ready for you to use
+*/
